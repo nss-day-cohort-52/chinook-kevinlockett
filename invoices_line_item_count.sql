88c4230e1116da6b3e5f4ee35720297b8aa8622d
Kevin Lockett <chlockett@msn.com>
2022-02-21 15:29:44 -0600

    kll-#18 sales_agent_total.sql

---
+++ invoices_line_item_count.sql
@@ -0,0 +1,9 @@
+--Provide a query that shows all Invoices. The resultant table should include:
+-- -InvoiceId
+-- -The total number of line items on each invoice
+
+SELECT I.InvoiceId, COUNT(IL.InvoiceLineId) AS LineItemCount
+FROM Invoice AS I
+JOIN InvoiceLine AS IL
+    ON I.InvoiceId = IL.InvoiceId
+GROUP BY I.InvoiceId
--- total_sales_{year}.sql
+++ total_sales_{year}.sql
@@ -1,7 +1,7 @@
 --What are the respective total sales for 2009 and 2011?
 
 SELECT strftime('%Y', InvoiceDate) as Year,
-    SUM(TOTAL) as YearlyTotal
+    ROUND(SUM(TOTAL), 2) as YearlyTotal
 FROM Invoice
 WHERE strftime('%Y', InvoiceDate) = '2009'
     OR strftime('%Y', InvoiceDate) = '2011'
