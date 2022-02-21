e05c7b03321823e2f84c88c1d087a82bd7f06bbb
Kevin Lockett <chlockett@msn.com>
2022-02-21 15:10:24 -0600

    kll-#16 tracks_no_id.sql

---
+++ tracks_no_id.sql
@@ -0,0 +1,16 @@
+-- Provide a query that shows all the Tracks, but displays no IDs. The resultant table should include:
+-- -Album name
+-- -Media type
+-- -Genre
+
+SELECT T.Name AS Track,
+    A.Title AS Album,
+    M.Name AS MediaType,
+    G.Name AS Genre
+FROM Track AS T
+JOIN MediaType AS M
+    ON T.MediaTypeId = M.MediaTypeId
+JOIN Album AS A
+    ON T.AlbumId = A.AlbumId
+JOIN Genre AS G
+    ON T.GenreId = G.GenreId
