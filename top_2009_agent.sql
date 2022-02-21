f676152d636bdd01d529695923949151d157b2ae
Kevin Lockett <chlockett@msn.com>
2022-02-21 15:44:11 -0600

    kll-#19 top_2009_agent.sql

---
+++ top_2009_agent.sql
@@ -0,0 +1,13 @@
+--Which sales agent made the most in sales in 2009?
+
+SELECT FirstName, LastName, ROUND(MAX(Total), 2) AS TotalSales
+FROM (
+    SELECT E.FirstName, E.LastName, SUM(I.Total) AS Total
+    FROM Employee AS E
+    JOIN Customer AS C
+        ON E.EmployeeId = C.SupportRepId
+    JOIN Invoice AS I
+        ON C.CustomerId = I.CustomerId
+    WHERE strftime('%Y', InvoiceDate) = '2009'
+    GROUP BY E.EmployeeId
+)
